ac2ac57f5de05ec0deab8ea473468e5d3ce81897
jonathanw2004 <jonathanw2004@hotmail.com>
2014-01-10 10:09:35 -0800

    added testng.xml and src files

---
+++ src/main/java/com/vmware/moviebase/App.java
@@ -0,0 +1,13 @@
+package com.vmware.moviebase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
